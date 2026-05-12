16153741c05ef0eaf6acd9295df2956da98a1a10
Diya Chauhan <diyac490@gmail.com>
2026-05-12 18:43:29 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       setResult(null);
 
       const response = await axios.post(
-        "http://localhost:5000/api/resume/upload",
+        "https://cvision-ai.onrender.com/api/resume/upload",
         formData
       );
 
